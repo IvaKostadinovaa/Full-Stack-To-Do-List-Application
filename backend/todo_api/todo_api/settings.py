1875028009490f100656131906ce09d4fd53baf5
IvaKostadinovaa <141180792+IvaKostadinovaa@users.noreply.github.com>
2025-06-04 16:05:33 +0200

    Update settings.py

--- backend/todo_api/todo_api/settings.py
+++ backend/todo_api/todo_api/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-8uuf7tazmiyjysliqnt6*oysc@r4(s3#^4hulgw_zvdyn3k+6c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
